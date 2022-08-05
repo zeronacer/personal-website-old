f45b79d0ec9dc7bcb82cd6213c7535dfc86b3a8a
zeronacer <max.grabau@outlook.de>
2022-08-05 12:16:42 +0200

    Add basic page layout with styles, Add navigation component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 import React from "react"
 import { graphql } from "gatsby"
+import Layout from "../components/layout"
 import ProjectLink from "../components/project-link"
 
 const IndexPage = ({
@@ -11,7 +12,24 @@ const IndexPage = ({
     .filter(edge => !!edge.node.frontmatter.date) // You can filter your posts based on some criteria
     .map(edge => <ProjectLink key={edge.node.id} project={edge.node} />)
 
-  return <div>{Projects}</div>
+  return (
+		<Layout>
+			<section id="home">
+				<div className="content">
+          <h1 className="">Maximilian Grabau</h1>
+        </div>
+			</section>
+			<section id="about">
+				<div class="content">About</div>
+			</section>
+			<section id="projects">
+				<div class="content">Projects{Projects}</div>
+			</section>
+			<section id="contact">
+				<div class="content">Contact</div>
+			</section>
+		</Layout>
+	);
 }
 
 export default IndexPage
