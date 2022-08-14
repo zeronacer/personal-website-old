7eb072e5c1c7c97849e90854823ecc167acd3714
zeronacer <max.grabau@outlook.de>
2022-08-14 18:38:33 +0200

    Reformat file

--- src/components/seo.js
+++ src/components/seo.js
@@ -1,11 +1,11 @@
 import React from "react";
 
-export default function SEO({children}) {
-    return(
-        <>
-            <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
-            <link rel="icon" type="image/png" href="/favicon.png" />
-            {children}
-        </>
-    )
+export default function SEO({ children }) {
+  return (
+    <>
+      <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+      <link rel="icon" type="image/png" href="/favicon.png" />
+      {children}
+    </>
+  );
 }
