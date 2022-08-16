cb7ef67efdd0e280d9d33a3400fa7b653bbb2508
zeronacer <max.grabau@outlook.de>
2022-08-16 22:38:58 +0200

    Add button styles, Add contact text and links

--- src/pages/index.js
+++ src/pages/index.js
@@ -104,6 +104,63 @@ const IndexPage = ({
       <section id="contact" className="bg-primary">
         <div className="content">
           <h2>Kontakt</h2>
+          <p>
+            Denkst du, dass meine Ambitionen zu deiner Firma und/oder deinem
+            Projekt passen? Dann schreib mir gern eine Nachricht!
+          </p>
+          <div className="button-group">
+            <a href="mailto:max@grabau.dev" className="button">
+              <svg
+                viewBox="0 0 24 24"
+                width="24"
+                height="24"
+                stroke="currentColor"
+                stroke-width="2"
+                fill="none"
+              >
+                <circle cx="12" cy="12" r="4"></circle>
+                <path d="M16 8v5a3 3 0 0 0 6 0v-1a10 10 0 1 0-3.92 7.94"></path>
+              </svg>
+              Email
+            </a>
+            <a
+              href="https://www.xing.com/profile/Maximilian_Grabau"
+              target="_blank"
+              rel="noreferrer"
+              className="button"
+            >
+              <svg
+                viewBox="0 0 24 24"
+                width="24"
+                height="24"
+                stroke="currentColor"
+                stroke-width="2"
+                fill="none"
+              >
+                <path d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2"></path>
+                <circle cx="12" cy="7" r="4"></circle>
+              </svg>
+              Xing
+            </a>
+            <a
+              href="https://github.com/zeronacer"
+              target="_blank"
+              rel="noreferrer"
+              className="button"
+            >
+              <svg
+                viewBox="0 0 24 24"
+                width="24"
+                height="24"
+                stroke="currentColor"
+                stroke-width="2"
+                fill="none"
+              >
+                <path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22"></path>
+              </svg>
+              GitHub
+            </a>
+          </div>
         </div>
       </section>
     </Layout>
