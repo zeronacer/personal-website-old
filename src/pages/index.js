e71c4d29d2d0904bd519a540d3c04568c920ca34
zeronacer <max.grabau@outlook.de>
2022-08-17 08:05:12 +0200

    Fix incorrect property of svg icons

--- src/pages/index.js
+++ src/pages/index.js
@@ -115,7 +115,7 @@ const IndexPage = ({
                 width="24"
                 height="24"
                 stroke="currentColor"
-                stroke-width="2"
+                strokeWidth="2"
                 fill="none"
               >
                 <circle cx="12" cy="12" r="4"></circle>
@@ -134,7 +134,7 @@ const IndexPage = ({
                 width="24"
                 height="24"
                 stroke="currentColor"
-                stroke-width="2"
+                strokeWidth="2"
                 fill="none"
               >
                 <path d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2"></path>
@@ -153,7 +153,7 @@ const IndexPage = ({
                 width="24"
                 height="24"
                 stroke="currentColor"
-                stroke-width="2"
+                strokeWidth="2"
                 fill="none"
               >
                 <path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22"></path>
